f7aa4cf3a2663bf8a0df68a45039c7cffe6b2499
LakiDIV <lakshikamadhuranga21@gmail.com>
2021-08-24 18:53:06 +0530

    Basic login and signup UI

---
+++ Artify Pixels/Artify Pixels/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿
+namespace Artify_Pixels
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.appVersion = new System.Windows.Forms.Label();
+            this.loginBtn = new System.Windows.Forms.Button();
+            this.userNameTxt = new System.Windows.Forms.TextBox();
+            this.passwordTxt = new System.Windows.Forms.TextBox();
+            this.signUpBtn = new System.Windows.Forms.Button();
+            this.greetingLbl = new System.Windows.Forms.Label();
+            this.isCaps = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.exitBtn.Location = new System.Drawing.Point(713, 415);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(75, 23);
+            this.exitBtn.TabIndex = 0;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = false;
+            this.exitBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Artify_Pixels.Properties.Resources.ArtfyPixels_LOGO_Transparent;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(227, 209);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // appVersion
+            // 
+            this.appVersion.AutoSize = true;
+            this.appVersion.Font = new System.Drawing.Font("Yu Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.appVersion.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.appVersion.Location = new System.Drawing.Point(12, 423);
+            this.appVersion.Name = "appVersion";
+            this.appVersion.Size = new System.Drawing.Size(66, 16);
+            this.appVersion.TabIndex = 3;
+            this.appVersion.Text = "Alpha 21.8";
+            // 
+            // loginBtn
+            // 
+            this.loginBtn.Location = new System.Drawing.Point(86, 308);
+            this.loginBtn.Name = "loginBtn";
+            this.loginBtn.Size = new System.Drawing.Size(75, 23);
+            this.loginBtn.TabIndex = 2;
+            this.loginBtn.Text = "Log In";
+            this.loginBtn.UseVisualStyleBackColor = true;
+            this.loginBtn.Click += new System.EventHandler(this.loginBtn_Click);
+            // 
+            // userNameTxt
+            // 
+            this.userNameTxt.Location = new System.Drawing.Point(39, 240);
+            this.userNameTxt.Name = "userNameTxt";
+            this.userNameTxt.Size = new System.Drawing.Size(176, 23);
+            this.userNameTxt.TabIndex = 4;
+            this.userNameTxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // passwordTxt
+            // 
+            this.passwordTxt.Location = new System.Drawing.Point(39, 269);
+            this.passwordTxt.MaxLength = 40;
+            this.passwordTxt.Name = "passwordTxt";
+            this.passwordTxt.PasswordChar = '°';
+            this.passwordTxt.ShortcutsEnabled = false;
+            this.passwordTxt.Size = new System.Drawing.Size(176, 23);
+            this.passwordTxt.TabIndex = 5;
+            this.passwordTxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.passwordTxt.TextChanged += new System.EventHandler(this.passwordTxt_TextChanged);
+            // 
+            // signUpBtn
+            // 
+            this.signUpBtn.Location = new System.Drawing.Point(86, 370);
+            this.signUpBtn.Name = "signUpBtn";
+            this.signUpBtn.Size = new System.Drawing.Size(75, 23);
+            this.signUpBtn.TabIndex = 6;
+            this.signUpBtn.Text = "Sign Up";
+            this.signUpBtn.UseVisualStyleBackColor = true;
+            // 
+            // greetingLbl
+            // 
+            this.greetingLbl.AutoSize = true;
+            this.greetingLbl.Font = new System.Drawing.Font("Ink Free", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.greetingLbl.Location = new System.Drawing.Point(281, 12);
+            this.greetingLbl.Name = "greetingLbl";
+            this.greetingLbl.Size = new System.Drawing.Size(107, 43);
+            this.greetingLbl.TabIndex = 7;
+            this.greetingLbl.Text = "label1";
+            this.greetingLbl.Click += new System.EventHandler(this.greetingLbl_Click);
+            // 
+            // isCaps
+            // 
+            this.isCaps.AutoSize = true;
+            this.isCaps.Font = new System.Drawing.Font("Yu Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.isCaps.ForeColor = System.Drawing.Color.DimGray;
+            this.isCaps.Location = new System.Drawing.Point(221, 272);
+            this.isCaps.Name = "isCaps";
+            this.isCaps.Size = new System.Drawing.Size(0, 16);
+            this.isCaps.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.isCaps);
+            this.Controls.Add(this.greetingLbl);
+            this.Controls.Add(this.signUpBtn);
+            this.Controls.Add(this.loginBtn);
+            this.Controls.Add(this.passwordTxt);
+            this.Controls.Add(this.appVersion);
+            this.Controls.Add(this.userNameTxt);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.exitBtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label appVersion;
+        private System.Windows.Forms.Button loginBtn;
+        private System.Windows.Forms.TextBox userNameTxt;
+        private System.Windows.Forms.TextBox passwordTxt;
+        private System.Windows.Forms.Button signUpBtn;
+        private System.Windows.Forms.Label greetingLbl;
+        private System.Windows.Forms.Label isCaps;
+    }
+}
+
---
+++ Artify Pixels/Artify Pixels/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Artify_Pixels
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+            // Greeting 
+            greetingLbl.Text = "Good Day !";
+            this.Controls.Add(greetingLbl);
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Promt a dialog box before exit the program
+            const string message = "Do you want to exit?";
+            const string caption = "EXIT";
+            var result = MessageBox.Show(message, caption, MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+            else if (result == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+        private void greetingLbl_Click(object sender, EventArgs e)
+        { }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        { }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        private void loginBtn_Click(object sender, EventArgs e)
+        {
+            
+            if (userNameTxt.Text == "user" && passwordTxt.Text == "pass")
+            {
+                Label loginLab = new Label();
+                loginLab.Text = "You loged in !";
+                this.Controls.Add(loginLab);
+            }
+            else
+            {
+                Label loginLab = new Label();
+                loginLab.Text = "Invalid username or password !";
+                this.Controls.Add(loginLab);
+            }
+        }
+
+        private void passwordTxt_TextChanged(object sender, EventArgs e)
+        {
+
+            if (Control.IsKeyLocked(Keys.CapsLock))
+            {
+                isCaps.Text = "Caps Lock is ON";
+                this.Controls.Add(isCaps);
+            }
+            else
+            {
+                isCaps.Text = "";
+                this.Controls.Add(isCaps);
+            }
+        }
+    }
+}
